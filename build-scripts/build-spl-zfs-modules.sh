834b9ab33e77ccd0aefbc4ce8a54c64536b6c0d2
Shengjing Zhu <zsj950618@gmail.com>
2017-04-27 18:50:49 +0800

    update spl, zfs binary build

--- build-scripts/build-spl-zfs-modules.sh
+++ build-scripts/build-spl-zfs-modules.sh
@@ -3,22 +3,25 @@ apt-get install git && apt-get build-dep zfs-linux spl-linux
 git clone git://anonscm.debian.org/pkg-zfsonlinux/zfs.git
 git clone git://anonscm.debian.org/pkg-zfsonlinux/spl.git
 
-dpkg -i linux-headers-3.16.36-ustclugsigned+deb8u2_3.16.36-ustclugsigned+deb8u2-1_amd64.deb
+dpkg -i linux-headers-4.9.18-ustclugsigned_4.9.18-ustclugsigned-3_amd64.deb
 
-export KVERS=3.16.36-ustclugsigned+deb8u2
-export KSRC=/usr/src/linux-headers-3.16.36-ustclugsigned+deb8u2
+export KVERS=4.9.18-ustclugsigned
+export KSRC=/usr/src/linux-headers-$KVERS
 
 export SPLOBJ=$(pwd)/spl
+export SPL=$(pwd)/spl
 
-export SIGNFILE=/usr/src/linux-headers-3.16.36-ustclugsigned+deb8u2/scripts/sign-file
+export SIGNFILE=$KSRC/scripts/sign-file
 export HASH=sha512
-export KEY=/build/signing_key_extra.priv
-export CERT=/build/signing_key_extra.x509
+export KEY=/root/signing_key_extra.priv
+export CERT=/root/signing_key_extra.x509
 
 cd spl
 patch -p1 < add-spl-sign.patch
+patch -p1 < spl-postinst.patch
 ./debian/rules override_dh_binary-modules
 
 cd zfs
 patch -p1 < add-zfs-sign.patch
+patch -p1 < zfs-postinst.patch
 ./debian/rules override_dh_binary-modules
